4e4d2aee48765b9d8855ac5b8340ac119f807d7b
Jaime Bernardo <jaime@janeasystems.com>
2018-02-07 20:25:49 +0000

    plugin: improve helper script shell compatibility
    Improves the shell compatibility of the sample project copy script by
    using the POSIX shell standard's syntax for defining the copy
    function. Also changes the copy function to a more specific name.

--- install/sample-project/copy-sample-project.sh
+++ install/sample-project/copy-sample-project.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 SRC_PATH="plugins/nodejs-mobile-cordova/install/sample-project"
 
-function copy {
+copySampleFile () {
   cp -i "$SRC_PATH/$1" $1
 }
 
 mkdir -p "www/nodejs-project"
 
-copy "www/js/index.js"
-copy "www/nodejs-project/main.js"
-copy "www/nodejs-project/package.json"
+copySampleFile "www/js/index.js"
+copySampleFile "www/nodejs-project/main.js"
+copySampleFile "www/nodejs-project/package.json"
