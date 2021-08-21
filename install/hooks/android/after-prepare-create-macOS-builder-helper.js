1788d4baf501c7054f97d745b9093a48a1af0ba3
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-08-21 18:57:59 +0900

    feat: support cordova-android@10

--- install/hooks/android/after-prepare-create-macOS-builder-helper.js
+++ install/hooks/android/after-prepare-create-macOS-builder-helper.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 // Gets the platform's www path.
-function getPlatformWWWPath(context, platform)
-{
-  var platformPath = path.join(context.opts.projectRoot, 'platforms', platform);
-  var platformAPI = require(path.join(platformPath, 'cordova', 'Api'));
-  var platformAPIInstance = new platformAPI();
+function getPlatformWWWPath(context, platform) {
+  const platformPath = path.join(context.opts.projectRoot, 'platforms', platform);
+  const platformAPI = require(path.join(platformPath, 'cordova', 'Api'));
+  let platformAPIInstance;
+  try {
+    platformAPIInstance = new platformAPI();
+  } catch (e) {
+    platformAPIInstance = new platformAPI(platform, platformPath);
+  }
   return platformAPIInstance.locations.www;
 }
+
 
 // Adds a helper script to run "npm rebuild" with the current PATH.
 // This workaround is needed for Android Studio on macOS when it is not started
